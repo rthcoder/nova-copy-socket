8b07b2064823dec3a635b03c7a8840fdce969531
Tohir <tohirdavrsmart@gmail.com>
2024-07-29 10:53:10 +0500

    finish stats socket

--- src/routers/socket.route.js
+++ src/routers/socket.route.js
@@ -4,5 +4,6 @@ import panel from "../controllers/socket.controller.js";
 const router = Router();
 
 router.post("/api/socket/panel", panel.PANEL);
+router.post("/api/socket/stats", panel.STATS);
 
 export default router;
